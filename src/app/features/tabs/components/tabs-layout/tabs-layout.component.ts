b93a4f2a9a2ca04fd3ee4a68623e77f1963b8bb0
Sham V. Fiorin <sham.vinicius@gmail.com>
2025-10-10 13:15:07 -0300

    feat: tabs rounded

--- src/app/features/tabs/components/tabs-layout/tabs-layout.component.ts
+++ src/app/features/tabs/components/tabs-layout/tabs-layout.component.ts
@@ -15,6 +15,26 @@ interface TabItem {
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './tabs-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [`
+    .active-tab .tab-button {
+      background: linear-gradient(to bottom right, rgb(59 130 246), rgb(37 99 235));
+      color: white;
+      box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
+      transform: scale(1.1);
+    }
+    .active-tab span:last-child {
+      color: rgb(37 99 235);
+      font-weight: 600;
+    }
+    @media (prefers-color-scheme: dark) {
+      .active-tab .tab-button {
+        background: linear-gradient(to bottom right, rgb(37 99 235), rgb(29 78 216));
+      }
+      .active-tab span:last-child {
+        color: rgb(96 165 250);
+      }
+    }
+  `]
 })
 export class TabsLayoutComponent {
   private router = inject(Router);
