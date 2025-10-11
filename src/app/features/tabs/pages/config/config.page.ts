8337f24a5e6415f35b016a62952f6b73b3a462bb
Sham V. Fiorin <sham.vinicius@gmail.com>
2025-10-11 11:55:24 -0300

    slash

--- src/app/features/tabs/pages/config/config.page.ts
+++ src/app/features/tabs/pages/config/config.page.ts
@@ -1,9 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Accordion, AccordionModule } from 'primeng/accordion';
+import { Textarea } from 'primeng/textarea';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'f-tabs-config-page',
   standalone: true,
+  imports: [FormsModule, AccordionModule, Textarea, ButtonModule],
   templateUrl: './config.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ConfigPage {}
+export default class ConfigPage {
+  readonly semanalText = signal('');
+  readonly mensalText = signal('');
+
+  finalizarSemanal() {
+    console.log('Semanal finalizado:', this.semanalText());
+  }
+
+  finalizarMensal() {
+    console.log('Mensal finalizado:', this.mensalText());
+  }
+}
